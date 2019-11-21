30e731774eb1f4e10f4bd2144dbd0367b4b776f3
s780609 <54741660+s780609@users.noreply.github.com>
2019-11-21 21:45:21 +0800

    開始製作HIGHCHART

---
+++ PMSAWebMVC/Controllers/SupplierController/SupplierHomePageController.cs
@@ -0,0 +1,36 @@
+﻿using PMSAWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PMSAWebMVC.Controllers.SupplierController
+{
+    public class SupplierHomePageController : Controller
+    {
+        PMSAEntities db;
+        public SupplierHomePageController()
+        {
+            db = new PMSAEntities();
+
+        }
+        // GET: SupplierHomePage
+        //供應商首頁
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //highChart
+        public ActionResult SupplierHomePage()
+        {
+            var q = from sl in db.SourceList
+                    select new
+                    {
+                        sl.PartNumber,
+                        sl.SafetyQty
+                    };
+            return Json(q,JsonRequestBehavior.AllowGet);
+        }
+    }
+}

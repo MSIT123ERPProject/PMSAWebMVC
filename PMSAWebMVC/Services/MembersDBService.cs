2d66a9057203c3aa0043965e44808e2bfa9e2df4
kiwijang <kiwijang5473@gmail.com>
2019-11-26 17:49:52 +0800

    1126
    刪掉遷移 / membersDBService.cs / UsersAdminViewModel.cs / UserAdmin-Index.cshtml

---
+++ PMSAWebMVC/Services/MembersDBService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Security;
+
+namespace PMSAWebMVC.Services
+{
+    public class MembersDBService
+    {
+    }
+}
---
+++ PMSAWebMVC/ViewModels/UsersAdmin/UsersAdminViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PMSAWebMVC.ViewModels.UsersAdmin
+{
+    public class UsersAdminViewModel
+    {
+    }
+}

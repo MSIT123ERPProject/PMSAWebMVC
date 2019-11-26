e55a185223e01f454abc098b2b1d4e68a88eca63
kiwijang <kiwijang5473@gmail.com>
2019-11-26 14:42:54 +0800

    1126 增加欄位 LastPasswordChangedDate/IsApproved

--- PMSAWebMVC/Models/IdentityModels.cs
+++ PMSAWebMVC/Models/IdentityModels.cs
@@ -24,6 +24,10 @@ namespace PMSAWebMVC.Models
         [StringLength(30)]
         public string RealName { get; set; }
 
+        public DateTime? LastPasswordChangedDate { get; set; }
+
+        public bool? IsApproved { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // 注意 authenticationType 必須符合 CookieAuthenticationOptions.AuthenticationType 中定義的項目

60b9b726aa8fbbf0d177fa12795fb44c9cc48b30
sonicTsai <sonictsailogin@gmail.com>
2019-12-02 19:12:20 +0800

    新增 第二種 更多 功能按鈕 顯示方式

--- PMSAWebMVC/Scripts/Site.js
+++ PMSAWebMVC/Scripts/Site.js
@@ -3,7 +3,7 @@
     "use strict";
 
     try {
-        $("body").tooltip({ selector: '[data-toggle=tooltip]' });
+        $("body").tooltip({ selector: '[data-toggle=tooltip]', placement: "top" });
         //$('[data-toggle="tooltip"]').tooltip();
     } catch (error) {
         console.log(error);

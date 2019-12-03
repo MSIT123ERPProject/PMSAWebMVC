bf3dd101309e36a170600315de7b27acd5fd67c7
sonicTsai <sonictsailogin@gmail.com>
2019-12-03 15:58:43 +0800

    完成 Contact 範例頁面

--- PMSAWebMVC/Scripts/Site.js
+++ PMSAWebMVC/Scripts/Site.js
@@ -5,6 +5,14 @@
     try {
         $("body").tooltip({ selector: '[data-toggle=tooltip]', placement: "top" });
         //$('[data-toggle="tooltip"]').tooltip();
+
+        $("body").popover({
+            selector: '[data-toggle="popover-dropdown"]', html: true,
+            trigger: 'focus',
+            placement: 'bottom',
+            content: function () { return $(this).children(".dropdown-menu").html(); }
+        });
+
     } catch (error) {
         console.log(error);
     }

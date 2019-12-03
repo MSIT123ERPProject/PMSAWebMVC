b46657a3346284dde39d075f207bd986e345c7cf
Howardmsit12328 <howard5737@gmail.com>
2019-12-03 20:46:09 +0800

    新增入庫單的Metadata

---
+++ PMSAWebMVC/Metadata/StockInDtlMetadata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PMSAWebMVC.Models
+{
+    public class StockInDtlMetadata
+    {
+        [Display(Name ="編號")]
+        public int StockInDtlOID { get; set; }
+
+        [Display(Name = "入庫單號")]
+        public string StockInID { get; set; }
+
+        [Display(Name = "庫存編號")]
+        public string InventoryCode { get; set; }
+
+        [Display(Name = "料件編號")]
+        public string PartNumber { get; set; }
+
+        [Display(Name = "入庫數量")]
+        public int StockInQty { get; set; }
+
+        [Display(Name = "保存期限")]
+        public Nullable<System.DateTime> EXP { get; set; }
+
+        [Display(Name = "備註")]
+        public string Remark { get; set; }
+    }
+}

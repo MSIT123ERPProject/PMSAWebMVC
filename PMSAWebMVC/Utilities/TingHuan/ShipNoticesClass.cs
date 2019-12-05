f70236f2aee46939e13f8fec0e63404f67f673c2
Tinghuan <s780609@gmail.com>
2019-12-06 01:07:55 +0800

    寄信功能製作中，想先改來做可互動式的首頁

--- PMSAWebMVC/Utilities/TingHuan/ShipNoticesClass.cs
+++ PMSAWebMVC/Utilities/TingHuan/ShipNoticesClass.cs
@@ -12,6 +12,22 @@ namespace PMSAWebMVC.Utilities.TingHuan
 {
     public class ShipNoticesUtilities
     {
+        public ShipNoticesUtilities(ApplicationUserManager userManager)
+        {
+            UserManager = userManager;
+        }
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
         private PMSAEntities db = new PMSAEntities();
         //找出該purchaseOrderID最新一筆異動資料
         public int FindPOChangedOID(string requesterRole, string purchaseOrderID)

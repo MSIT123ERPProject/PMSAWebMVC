8b59766569f9a849816b878908afbb0e96e89845
sonicTsai <sonictsailogin@gmail.com>
2019-12-08 05:49:17 +0800

    新增 輪播功能 UI

--- PMSAWebMVC/App_Start/BundleConfig.cs
+++ PMSAWebMVC/App_Start/BundleConfig.cs
@@ -9,7 +9,9 @@ namespace PMSAWebMVC
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery-migrate/jquery-migrate-3.1.0.js"
+                ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.validate*"));

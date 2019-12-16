bcce765e73bf1b047f3839da5a4c20fda03368d3
sonicTsai <sonictsailogin@gmail.com>
2019-12-16 19:04:38 +0800

    交易記錄功能完成

--- PMSAWebMVC/ViewModels/PurchaseOrders/PurchaseOrderIndexViewModel.cs
+++ PMSAWebMVC/ViewModels/PurchaseOrders/PurchaseOrderIndexViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using PMSAWebMVC.Models;
+using PMSAWebMVC.Utilities.YaChen;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -67,40 +68,12 @@ namespace PMSAWebMVC.ViewModels.PurchaseOrders
                            CreateDate = gp.Key.CreateDate,
                            SupplierName = gp.Key.SupplierName,
                            PurchaseRequisitionID = gp.Key.PurchaseRequisitionID,
-                           PurchaseOrderStatus = GetStatus(gp.Key.PurchaseOrderStatus)
+                           PurchaseOrderStatus = RepositoryUtils.GetStatus(gp.Key.PurchaseOrderStatus)
                        };
             return povm;
         }
 
-        private string GetStatus(string purchaseOrderStatus)
-        {
-            //N = 新增,P = 送出,C = 異動中,E = 答交,D = 整筆訂單取消,S = 出貨,R = 點交,O = 逾期,Z = 結案
-            switch (purchaseOrderStatus)
-            {
-                case "N":
-                    return "新增";
-                case "P":
-                    return "送出";
-                case "C":
-                    return "異動中";
-                case "E":
-                    return "答交";
-                case "W":
-                    return "雙方答交";
-                case "D":
-                    return "取消";
-                case "S":
-                    return "出貨";
-                case "R":
-                    return "點交";
-                case "O":
-                    return "逾期";
-                case "Z":
-                    return "結案";
-                default:
-                    return "";
-            }
-        }
+        
 
     }
 }

e4b161ede541a30ed2c61b7305617803054f9d2c
kiwijang <kiwijang5473@gmail.com>
2019-12-16 19:05:10 +0800

    1216 檢查密碼是否正確的方法(簽合用)

---
+++ PMSAWebMVC/Utilities/YangTing/checkPwd.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PMSAWebMVC.Utilities.YangTing
+{
+    public class checkPwd
+    {
+        public static bool isCorrectPwd(ApplicationSignInManager SignInManager, string LoginId, string pwd)
+        {
+            //確定密碼是否正確
+            var result = SignInManager.PasswordSignIn(LoginId, pwd, false, false);
+
+            switch (result)
+            {
+                case SignInStatus.Success:
+                    return true;
+                case SignInStatus.Failure:
+                default:
+                    return false;
+            }
+        }
+    }
+}
